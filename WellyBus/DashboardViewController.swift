8b96570f49dfac36bce36b029d6429909d94735c
chrishawkinsnz <chrishawkinsnz@gmail.com>
2014-07-20 20:20:17 +1200

    idk

--- WellyBus/DashboardViewController.swift
+++ WellyBus/DashboardViewController.swift
@@ -6,17 +6,36 @@
 //  Copyright (c) 2014 Chris. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class DashboardViewController: UITableViewController {
+class DashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+   
+    @IBOutlet
+    var tableView: UITableView
+    var user:User!
+    
+    var refreshControl:UIRefreshControl!
+    
+    init(coder aDecoder: NSCoder!){
+        super.init(coder: aDecoder)
 
-    init(style: UITableViewStyle) {
-        super.init(style: style)
-        // Custom initialization
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.user = User()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        
+        //refreshing thign
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.backgroundColor = UIColor.purpleColor()
+        self.refreshControl.tintColor = UIColor.whiteColor()
+        self.refreshControl.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.addSubview(self.refreshControl)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,6 +43,19 @@ class DashboardViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    func tableView(tableView: UITableView!, titleForHeaderInSection section: Int) -> String! {
+        return self.user.favStops[section].name
+        
+    }
+    
+    func refresh(sender:AnyObject){
+        self.tableView.reloadData()
+        self.refreshControl.endRefreshing()
+    }
+    
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -32,18 +64,56 @@ class DashboardViewController: UITableViewController {
 
     // #pragma mark - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView?) -> Int {
+     func numberOfSectionsInTableView(tableView: UITableView?) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return self.user.favStops.count
     }
+    
 
-    override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+
+     func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return self.user.favStops[section].favouriteIncoming.count
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!{
+        var cell:myArrivaCelll = self.tableView.dequeueReusableCellWithIdentifier("dashboardCell") as myArrivaCelll
+        
+        var stop:BusStop = self.user.favStops[indexPath.section]
+        var arrival:Arrival = stop.favouriteIncoming[indexPath.row]
+        
+        cell.nameLabel.text = arrival.route.name
+        cell.numberLabel.text = String(arrival.route.number)
+        cell.numberLabel.textColor = arrival.route.color
+        cell.timeLabel.text = arrival.time
+//        cell.text = arrival.description
+
+        return cell
     }
 
+    
+    @IBAction func goToFavs(){
+        self.performSegueWithIdentifier("goToFavs", sender: nil)
+
+    }
+    
+    @IBAction func reload(){
+        self.tableView.reloadData()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!){
+        if segue.identifier=="goToFavs"{
+            let destination:AllStopsViewController = segue.destinationViewController as AllStopsViewController
+            
+            destination.user = self.user
+        }
+    }
     /*
     override func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath: NSIndexPath?) -> UITableViewCell? {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
--- WellyBus/MyPlayground.playground/section-1.swift
+++ WellyBus/MyPlayground.playground/section-1.swift
@@ -3,3 +3,7 @@
 import UIKit
 
 var str = "Hello, playground"
+var colStr = "e6b012"
+var hexNum:String = "e6"
+var n:Int = hexNum.toInt()!
+println(n)
